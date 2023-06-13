eff6d68a043f1afa1d67feb063237101462b0a81
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-06-13 14:40:39 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -27,8 +27,33 @@ class Solution
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       vector<vector<int>>dp(n,vector<int>(W+1,-1));
-       return f(n-1,W,wt,val,dp);
+    //   vector<vector<int>>dp(n,vector<int>(W+1,-1));
+       //   return f(n-1,W,wt,val,dp);
+       
+       
+       //Tabulation
+       vector<vector<int>>dp(n,vector<int>(W+1,0));
+       for(int i=wt[0];i<=W;i++){
+           dp[0][i]=val[0];
+       }
+       
+       for(int ind=1;ind<n;ind++){
+           
+           for(int w=0;w<=W;w++){
+               
+               int notTake=dp[ind-1][w];
+               int take=INT_MIN;
+               
+               if(wt[ind]<=w)take=val[ind]+dp[ind-1][w-wt[ind]];
+           
+            dp[ind][w]=max(take,notTake);
+               
+               
+           }
+          
+       }
+    
+    return dp[n-1][W];
     }
 };
 
