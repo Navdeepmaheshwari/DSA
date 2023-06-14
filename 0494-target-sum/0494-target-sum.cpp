a0a953a969ab56938d11f73fb17342f98d898508
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-06-14 14:28:34 +0530

    Time: 7 ms (96.60%), Space: 11.9 MB (48.46%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,45 @@
 class Solution {
 public:
-   int solve(int idx,int target,vector<int>& nums){
-        if(idx==nums.size()){
-            if(target==0){
-               return 1;
-            }
-            return 0;
+	int f(int ind,vector<int>& arr,int sum,vector<vector<int>>&dp ){
+
+	 if(ind==0){
+
+	   if(sum==0&&arr[0]==0)return 2;
+	   if(sum==0||sum==arr[ind])return 1;
+	   return 0;
+	 }
+	    
+	    if(dp[ind][sum]!=-1)return dp[ind][sum];
+	    int notTake=f(ind-1,arr,sum,dp);
+	    int take=0;
+	    if(sum>=arr[ind]){
+	        take=f(ind-1,arr,sum-arr[ind],dp);
+	    }
+	    
+	    return dp[ind][sum]=(take+notTake);
+	}
+	
+  	int perfectSum(vector<int>& arr, int sum)
+	{int n=arr.size();
+        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        return f(n-1,arr,sum,dp);
+	}
+	
+    int countPartitions(int n, int d, vector<int>& arr) {
+        // Code here
+        int total=0;
+        for(auto x:arr){
+            total+=x;
         }
-       
-       int pos=solve(idx+1,target-nums[idx],nums);
-       int neg= solve(idx+1,target+nums[idx],nums);
-       return pos+neg;
+        
+        
+        if(total-d<0||(total-d)%2)return 0;
+      
+        return perfectSum(arr,(total-d)/2);
+        
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(0,target,nums);
+        return countPartitions(nums.size(),target,nums);
         
     }
 };
