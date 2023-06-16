3ad38d640344f2e1bbec5acce5be4fff40534ac5
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-06-16 14:18:47 +0530

    Time: 179 ms (18.02%), Space: 45.1 MB (35.58%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -21,9 +21,38 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
       int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=f(n-1,amount,coins,dp);
-        if(ans>=1e9)return -1;
+        // vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        // int ans=f(n-1,amount,coins,dp);
+        // if(ans>=1e9)return -1;
+        // return ans;
+        
+        
+          
+        // Tabulation
+         vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        
+        for(int target=0;target<=amount;target++){
+           if(target%coins[0]==0)dp[0][target]=target/coins[0];
+            else dp[0][target]=1e9;
+        }
+        
+        for(int ind=1;ind<n;ind++){
+            
+            for(int target=0;target<=amount;target++){
+                
+                int notTake=0+dp[ind-1][target];
+                int take=INT_MAX;
+
+                if(coins[ind]<=target){
+                    take=1+dp[ind][target-coins[ind]];
+                }
+                
+                dp[ind][target]=min(notTake,take);
+                
+            }
+        }
+        int ans= dp[n-1][amount];
+    if(ans>=1e9)return -1;
         return ans;
     }
 };
