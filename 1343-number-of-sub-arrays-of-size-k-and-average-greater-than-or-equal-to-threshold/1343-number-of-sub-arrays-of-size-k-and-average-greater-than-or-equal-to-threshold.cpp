c0dceda5cc923a3f29114af16b8e4d21818d5b60
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-06-20 11:56:10 +0530

    Time: 107 ms (32.30%), Space: 55.4 MB (47.24%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -4,19 +4,32 @@ public:
         
         int n=arr.size();
         int cnt=0;
-        vector<int>presum(n+1,0);
-        for(int i=1;i<=n;i++){
-            presum[i]=presum[i-1]+arr[i-1];
-        }
-            for(auto x:presum){
-            cout<<x<<" ";
-        }
-        cout<<endl;
         
-        for(int i=k;i<=n;i++){
+//         vector<int>presum(n+1,0);
+//         for(int i=1;i<=n;i++){
+//             presum[i]=presum[i-1]+arr[i-1];
+//         }
+//             for(auto x:presum){
+//             cout<<x<<" ";
+//         }
+//         cout<<endl;
+        
+//         for(int i=k;i<=n;i++){
             
-            int avg=(presum[i]-presum[i-k])/k;
-            if(avg>=threshold)cnt++;
+//             int avg=(presum[i]-presum[i-k])/k;
+//             if(avg>=threshold)cnt++;
+//         }
+        
+        int sum=0;
+        
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            
+            if(i>=k-1){
+                int avg=sum/k;
+                if(avg>=threshold)cnt++;
+                sum-=arr[i-(k-1)];
+            }
         }
         
         return cnt;
