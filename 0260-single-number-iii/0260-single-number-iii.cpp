795eae4c79637b5d0902bd44f710139eaf9169b1
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-07-04 15:35:22 +0530

    Time: 19 ms (10.24%), Space: 10.6 MB (33.25%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -2,41 +2,14 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         
-//         unordered_map<int,int>mp;
-//         vector<int>ans;
-//         for(auto x:nums){
-//             mp[x]++;
-//         }
-//         for(auto i:mp){
-//             if(i.second==1)ans.push_back( i.first);
-//         }
-//         return ans;
-        
-        int XOR=0;
-        int first=0;
+        unordered_map<int,int>mp;
+        vector<int>ans;
         for(auto x:nums){
-            XOR^=x;
+            mp[x]++;
         }
-        // cout<<XOR<<endl;
-        
-        // get rightmost set bit in above xor
-        int mask=1;
-        while(!(XOR&mask)){
-            mask=mask<<1;
+        for(auto i:mp){
+            if(i.second==1)ans.push_back( i.first);
         }
-        
-        //using mask finding the first num
-        for(auto &x:nums){
-            // cout<<x<<" ";
-            if(x&mask){
-                // cout<<x<<endl;
-                first=first^x;
-            }
-            // cout<<endl;
-        }
-        
-       
-        return {first,first^XOR};
+        return ans;
     }
-    
 };
