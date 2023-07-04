1a9347fe40730cc17f6e04e4819ba4e25f58b176
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-07-04 14:20:16 +0530

    Time: 9 ms (58.12%), Space: 9.4 MB (72.62%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,15 +2,29 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-        unordered_map<int,int>mp;
-        for(auto x:nums){
-            mp[x]++;
-        }
+//         unordered_map<int,int>mp;
+//         for(auto x:nums){
+//             mp[x]++;
+//         }
         
-        for(auto i:mp){
+//         for(auto i:mp){
             
-            if(i.second==1)return i.first;
+//             if(i.second==1)return i.first;
+//         }
+//         return -1;
+        
+        int res=0;
+        for(int i=0;i<32;i++){
+            
+            int cnt=0;
+            for(auto num:nums){
+                cnt+=((num>>i)&1);
+            }
+            // cout<<cnt<<endl;
+            cnt=cnt%3;
+            res=res|(cnt<<i);
         }
-        return -1;
+        return res;
+
     }
 };
