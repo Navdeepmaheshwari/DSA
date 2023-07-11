5dc2b7a209b03c2ab7a2f9a6e00f34fcdeb175c3
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-07-11 10:27:23 +0530

    Time: 682 ms (27.48%), Space: 185.6 MB (25.97%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   void graph(TreeNode*root, unordered_map<int,vector<int>>&mp){
+        if(root->left){
+            mp[root->val].push_back(root->left->val);
+            mp[root->left->val].push_back(root->val);
+            
+            graph(root->left,mp);
+        }
+        
+        if(root->right){
+            mp[root->val].push_back(root->right->val);
+            mp[root->right->val].push_back(root->val);
+            
+            graph(root->right,mp);
+        }
+    }
+    int amountOfTime(TreeNode* root, int start) {
+        
+        unordered_map<int,vector<int>>mp; //convert tree to graph first
+        int time=0;
+        unordered_map<int,bool> vis;
+        graph(root,mp);
+        
+        // for(auto x:mp){
+        //     cout<<x.first<<">";
+        //     for(auto y:x.second){
+        //         cout<<y<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        queue<int>q;
+        q.push(start);
+        while(!q.empty()){
+            int len=q.size();
+            for(int i=0;i<len;i++){
+                int node=q.front();q.pop();
+                vis[node]=true;
+                
+                for(auto x:mp[node]){
+                    if(!vis[x]){
+                        q.push(x);
+                    }
+                }
+            }
+            time++; 
+        }
+        return time-1;
+    }
+};
