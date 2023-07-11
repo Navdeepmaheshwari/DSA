d7aa576f92ef4b682cea68473efe8256d33ba867
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-07-11 10:19:06 +0530

    Time: 7 ms (65.63%), Space: 13.3 MB (28.63%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void graph(TreeNode*root, unordered_map<int,vector<int>>&mp){
+        
+        if(root->left){
+            mp[root->val].push_back(root->left->val);
+            mp[root->left->val].push_back(root->val);
+            
+            graph(root->left,mp);
+        }
+        
+        if(root->right){
+            mp[root->val].push_back(root->right->val);
+            mp[root->right->val].push_back(root->val);
+            
+            graph(root->right,mp);
+        }
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        
+         if(k==0)
+            return {target->val};
+        
+        unordered_map<int,vector<int>>mp; //convert tree to graph first
+        vector<int>ans;
+        unordered_map<int,bool> vis;
+        graph(root,mp);
+        // for(auto x:mp){
+        //     cout<<x.first<<">";
+        //     for(auto y:x.second){
+        //         cout<<y<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        //Now do bfs traversal
+        queue<int>q;
+        q.push(target->val);
+        while(!q.empty()){
+            
+            if(k==0){
+                break;
+            }
+            
+            k--;
+            int len=q.size();
+            for(int i=0;i<len;i++){
+                int node=q.front();q.pop();
+                vis[node]=true;
+                
+                for(auto x:mp[node]){
+                    if(!vis[x]){
+                        q.push(x);
+                    }
+                }
+            }
+            
+        }
+        
+        //queue contains the nodes val which is at distance k
+        while(!q.empty()){
+           ans.push_back(q.front());q.pop(); 
+        }
+        return ans;
+        
+    }
+};
