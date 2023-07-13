12ab00170c48dd051756e8296403aa4160b9c372
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-07-13 16:40:53 +0530

    Time: 817 ms (31.98%), Space: 207 MB (45.89%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -18,7 +18,33 @@ public:
         int n=p.size();
         int maxTransactions=2;
         //3D-DP
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(maxTransactions+1,-1)));
-        return f(0,1,p,dp,n,maxTransactions);
+        // vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(maxTransactions+1,-1)));
+        // return f(0,1,p,dp,n,maxTransactions);
+        
+        
+        //Tabulation
+   vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(maxTransactions+1,0)));
+        
+        //Base case 
+        //as base case in recursion return 0 and in tabulation we initialize dp with 0 
+        //so need to write;
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cnt=1;cnt<=maxTransactions;cnt++){//for cnt=0 always 0,so start from cnt=1
+                    
+                int profit = 0;
+                if( buy )//means we can buy, here two cases exist whether we want to buy or not
+                    profit = max( -p[ind]+ dp[ind+1][0][cnt], dp[ind+1][1][cnt] );
+                else
+                    profit = max( p[ind]+dp[ind+1][1][cnt-1] , dp[ind+1][0][cnt]);
+                
+                dp[ind][buy][cnt] = profit; 
+                    
+                }
+            }
+        }
+        return dp[0][1][maxTransactions];
+
     }
 };
