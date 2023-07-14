73e2f272818f2b683f4030bd0ae737e84435fd6d
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-07-14 14:00:29 +0530

    Time: 244 ms (38.23%), Space: 90.8 MB (35.42%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,24 +1,48 @@
 class Solution {
 public:
-    int f(int ind,int buy, vector<int>&p,int n,int fee, vector<vector<int>>&dp){
+//     int f(int ind,int buy, vector<int>&p,int n,int fee, vector<vector<int>>&dp){
         
-        if(ind>=n){
-            return 0;
-        }
-        if(dp[ind][buy]!=-1)return dp[ind][buy];
-        int profit=0;
-        if(buy){
-            profit=max(-p[ind]+f(ind+1,0,p,n,fee,dp), 0+f(ind+1,1,p,n,fee,dp));
-        }
-        else{
-            //stock sold only then fee applicable
-            profit=max(p[ind]-fee+f(ind+1,1,p,n,fee,dp),0+f(ind+1,0,p,n,fee,dp));
-        }
-        return dp[ind][buy]=profit;
-    }
+//         if(ind>=n){
+//             return 0;
+//         }
+//         if(dp[ind][buy]!=-1)return dp[ind][buy];
+//         int profit=0;
+//         if(buy){
+//             profit=max(-p[ind]+f(ind+1,0,p,n,fee,dp), 0+f(ind+1,1,p,n,fee,dp));
+//         }
+//         else{
+//             //stock sold only then fee applicable
+//             profit=max(p[ind]-fee+f(ind+1,1,p,n,fee,dp),0+f(ind+1,0,p,n,fee,dp));
+//         }
+//         return dp[ind][buy]=profit;
+//     }
     int maxProfit(vector<int>& p, int fee) {
-                int n=p.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return f(0,1,p,n,fee,dp);
+        int n=p.size();
+        
+        // vector<vector<int>>dp(n,vector<int>(2,-1));
+        // return f(0,1,p,n,fee,dp);
+        
+        
+          //Tabulation
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            
+            for(int buy=0;buy<=1;buy++){
+                
+                int profit=0;
+                if(buy){
+                    
+                    profit=max(-p[ind]+dp[ind+1][0],0+dp[ind+1][1]);
+                }
+                else{
+                    //stock sold only then fee applicable
+                    profit=max(p[ind]-fee+dp[ind+1][1],0+dp[ind+1][0]);
+                }
+                
+                dp[ind][buy]=profit;
+            }
+        }
+        return dp[0][1];
     }
 };
