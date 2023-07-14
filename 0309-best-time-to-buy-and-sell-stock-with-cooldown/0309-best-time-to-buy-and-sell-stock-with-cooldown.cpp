fb656bec6e688211ef8011ee47f86c90a1b355f5
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-07-14 13:48:17 +0530

    Time: 4 ms (76.13%), Space: 11.7 MB (65.29%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -5,7 +5,6 @@ public:
         if(ind>=n){
             return 0;
         }
-        
         if(dp[ind][buy]!=-1)return dp[ind][buy];
         int profit=0;
         if(buy){
@@ -20,7 +19,29 @@ public:
     int maxProfit(vector<int>& p) {
         
         int n=p.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return f(0,1,p,n,dp);
+        // vector<vector<int>>dp(n,vector<int>(2,-1));
+        // return f(0,1,p,n,dp);
+        
+        //Tabulation
+        vector<vector<int>>dp(n+2,vector<int>(2,0));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            
+            for(int buy=0;buy<=1;buy++){
+                
+                int profit=0;
+                if(buy){
+                    
+                    profit=max(-p[ind]+dp[ind+1][0],0+dp[ind+1][1]);
+                }
+                else{
+                    profit=max(p[ind]+dp[ind+2][1],0+dp[ind+1][0]);
+                }
+                
+                dp[ind][buy]=profit;
+            }
+        }
+        return dp[0][1];
+
     }
 };
