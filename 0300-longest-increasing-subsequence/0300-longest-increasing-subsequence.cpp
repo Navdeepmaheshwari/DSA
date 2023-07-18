c304aac6f6075be4e0be4c7c374fb2689060a624
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-07-18 15:30:23 +0530

    Time: 3 ms (99.76%), Space: 10.4 MB (66.88%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,50 +1,22 @@
 class Solution {
 public:
-//     int f(int curInd,int prevInd,vector<int>&nums,int n,vector<vector<int>>&dp){
-//         //Base Case
-//         if(curInd==n)return 0;
-        
-//         int len=0;
-        
-//         if(dp[curInd][prevInd+1]!=-1)return dp[curInd][prevInd+1];
-        
-//         //not Take
-//         len=0+f(curInd+1,prevInd,nums,n,dp);
-        
-//         //Take
-//         if(prevInd==-1||nums[prevInd]<nums[curInd]){
-//            len=max(len,1+f(curInd+1,curInd,nums,n,dp));
-//         }
-//         return dp[curInd][prevInd+1]=len;
-//     }
-    int lengthOfLIS(vector<int>& nums) {
-        
-        int n=nums.size();
-        // //coordinate chnage apply for prevInd because we cant store at index -1 in vector  
-        // vector<vector<int>>dp(n,vector<int>(n+1,-1)); 
-        // return f(0,-1,nums,n,dp);
-        
-        
-        //Tabulation
-         vector<vector<int>>dp(n+1,vector<int>(n+1,0)); 
-        
-        for(int curInd=n-1;curInd>=0;curInd--){
-            for(int prevInd=curInd-1;prevInd>=-1;prevInd--){
-                
-     //in dp second parameter always add +1 because of coordinate shifting
-
-                int len=0;
-                len=0+dp[curInd+1][prevInd+1];
-                
-                if(prevInd==-1||nums[prevInd]<nums[curInd]){
-                    len=max(len,1+dp[curInd+1][curInd+1]);
-                }
-                
-                dp[curInd][prevInd+1]=len;
-            }
-        }
-        return dp[0][-1+1];
-        
-
+    int lengthOfLIS(vector<int>& a) {
+        int n=a.size();
+        vector<int>v;
+       v.push_back(a[0]);
+       int len=1;
+       
+       for(int i=1;i<n;i++){
+           
+           if(a[i]>v.back()){
+               v.push_back(a[i]);
+               len++;
+           }
+           else{
+               int ind=lower_bound(v.begin(),v.end(),a[i])-v.begin();
+               v[ind]=a[i];
+           }
+       }
+       return len;
     }
 };
