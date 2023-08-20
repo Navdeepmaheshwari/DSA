19e677e0cc82d9a4615233aac483b98346985b1f
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-08-20 12:25:56 +0530

    Time: 395 ms (50.80%), Space: 10.4 MB (70.04%) - LeetHub

---
+++ 0923-3sum-with-multiplicity/0923-3sum-with-multiplicity.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int MOD=1e9+7;
+    int threeSumMulti(vector<int>& nums, int target) {
+        
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int ans=0;
+        for(int i=0;i<n;i++){        
+            int low=i+1,high=n-1;
+            while(low<high){
+                
+                 if(nums[i]+nums[low]+nums[high]==target){
+                    
+                    long long cntl=1,cnth=1;
+                     
+                     while(low<high&&nums[low]==nums[low+1]){
+                         low++;
+                         cntl++;
+                     }
+                     while(low<high&&nums[high]==nums[high-1]){
+                         high--;
+                         cnth++;
+                     }
+                     
+                     //if low==high mean element are same so nC2
+                     // 2 elements from n combinations,where n (cntl/cnth).
+                     if(low==high){
+                         long long curr=(cntl*(cntl-1))/2; //nC2
+                         ans=(ans+curr)%MOD;
+                     }
+                      //if low!=high simply multiply cntl*cnth;
+                     else{
+                         long long curr=cntl*cnth;
+                         ans=(ans+curr)%MOD;
+                     }
+                     
+                   low++;
+                   high--;
+                }
+                
+                 else if(nums[i]+nums[low]+nums[high]<target)  low++;
+                 else if(nums[i]+nums[low]+nums[high]>target)  high--;
+            }
+        }
+        return ans;
+    }
+};
