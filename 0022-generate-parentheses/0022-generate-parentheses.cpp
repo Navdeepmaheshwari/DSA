ef7bb524746488d5a7dd4b7dcf23c686795c37a5
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-08-21 18:32:11 +0530

    Time: 3 ms (71.51%), Space: 11.4 MB (83.56%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     void solve(int cntO,int cntC,string&res, vector<string>&ans,int n){
-        
+       
         if(cntO==0&&cntC==0){
             ans.push_back(res);
             return ;
         }
         
-        //cntO==cntC then we take first open bracket
-        if(cntO==cntC){
+      
+        if(cntO!=0){
             res.push_back('(');
             solve(cntO-1,cntC,res,ans,n);
             res.pop_back();
         }
         
-        //cntO==0 then we only have 1 option take close bracket
-        else if(cntO==0){
-            res.push_back(')');
-            solve(cntO,cntC-1,res,ans,n);
-            res.pop_back();
-        }
-
-        // else we have 2 options either open or close
-        else{
-            res.push_back('(');
-            solve(cntO-1,cntC,res,ans,n);
-            res.pop_back();
-            
+        
+         if(cntO<cntC){
             res.push_back(')');
             solve(cntO,cntC-1,res,ans,n);
             res.pop_back();
