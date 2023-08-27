f082555f671928c35303d67c73a3e203d8292336
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-08-27 17:54:56 +0530

    Time: 0 ms (100.00%), Space: 10.6 MB (32.69%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    void solve(int ind,int n,unordered_set<string>&st,string curr, vector<string>&ans){
-        
+    void solve(int ind,int n,unordered_set<string>&st,string curr,string&ans){
+        if(ans!="")return;
         if(ind==n){
             if(st.find(curr)==st.end())
-                ans.push_back(curr);
+                ans=curr;
               // cout<<curr<<endl;
             return;
         }
-        
+
         solve(ind+1,n,st,curr+"0",ans);
         solve(ind+1,n,st,curr+"1",ans);
         
     }
     string findDifferentBinaryString(vector<string>& nums) {
         int n=nums.size();
-        unordered_set<string>st;
-        
-        for(auto x:nums){
-            st.insert(x);
-        }
-        vector<string>ans;
+        unordered_set<string>st(nums.begin(),nums.end());
+        string ans="";
         solve(0,n,st,"",ans);
-        return ans[0];
-        
+        return ans;
     }
 };
