1be81331d3cff60fbfaf7709476e248d5b10c6b8
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-08-27 17:35:20 +0530

    Time: 14 ms (78.97%), Space: 17.9 MB (87.53%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,14 +2,27 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-        unordered_map<int,int>mp;
-        for(auto x:nums){
-            mp[x]++;
+        
+        int xor_nums=0;
+        int xor_indx=0;
+        
+        for(int i=0;i<n;i++){
+            xor_nums=xor_nums^nums[i];
+            xor_indx=xor_indx^i;
         }
         
-        for(int i=0;i<=n;i++){
-            if(mp.find(i)==mp.end())return i;
-        }
-        return -1;
+        xor_indx=xor_indx^n; //we have to take [0,n] that's why
+        
+        return xor_nums^xor_indx;
+        
+//         unordered_map<int,int>mp;
+//         for(auto x:nums){
+//             mp[x]++;
+//         }
+        
+//         for(int i=0;i<=n;i++){
+//             if(mp.find(i)==mp.end())return i;
+//         }
+//         return -1;
     }
 };
