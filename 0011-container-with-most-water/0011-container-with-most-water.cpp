e4806d904354422f97899346a8d9e0e92ec6c511
Navdeep Maheshwari <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-09-08 15:34:40 +0530

    Time: 78 ms (40.86%), Space: 59.2 MB (5.88%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int mxarea=0;
+        int n=height.size();
         int p1=0;
-        int p2=height.size()-1;
+        int p2=n-1;
+        int maxx=INT_MIN;
         
         while(p1<p2){
-            
             int h=min(height[p1],height[p2]);
-            int currarea=h*(p2-p1);
-             mxarea=max(mxarea, currarea);
+            int currArea=h*(p2-p1);
+            maxx=max(maxx,currArea);
             
-            while(height[p1]<=h && p1<p2) p1++;
-            while(height[p2]<=h && p1<p2) p2--;
+            // p1++;
+            // p2--;
+            if(height[p1]<=height[p2])p1++;
+            else p2--;
         }
-        return mxarea;
         
+        return maxx;
     }
 };
