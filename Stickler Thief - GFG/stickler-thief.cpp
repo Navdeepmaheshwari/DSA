c1635eaa70b0828f79f0a04f4b8f950b736afbd6
Navdeep Maheshwari <77584053+Navdeepmaheshwari@users.noreply.github.com>
2023-09-21 14:47:32 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.cpp
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -9,21 +9,34 @@ class Solution
     public:
     //Function to find the maximum money the thief can get.
     int solve(int i,int arr[],int n, vector<int>&dp){
-        if(i==n-1){
+        if(i==0){
             return arr[i];
         }
-        if(i>=n)return 0;
+        if(i<0)return 0;
         if(dp[i]!=-1)return dp[i];
-        int pick= arr[i]+solve(i+2,arr,n,dp);
-        int notPick=0+solve(i+1,arr,n,dp);
+        int pick= arr[i]+solve(i-2,arr,n,dp);
+        int notPick=0+solve(i-1,arr,n,dp);
         
         return dp[i]=max(pick,notPick);
     }
     int FindMaxSum(int arr[], int n)
     {
         // Your code here
-        vector<int>dp(n,-1);
-        return solve(0,arr,n,dp);
+        // vector<int>dp(n,-1);
+        // return solve(n-1,arr,n,dp);
+        
+         vector<int>dp(n,0);
+         dp[0]=arr[0];
+        dp[1]=max(arr[0],arr[1]);
+         for(int i=2;i<n;i++){
+             
+            int pick=arr[i]+dp[i-2];
+            int notPick=0+dp[i-1];
+            
+            dp[i]=max(pick,notPick);
+             
+         }
+         return dp[n-1];
     }
 };
 
